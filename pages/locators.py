88aaa5f78e254dc1b09c3822caac61362ba02bf5
Ann <astronettle@gmail.com>
2022-04-14 11:43:20 +0300

    добавлен переход на страницу логина

--- pages/locators.py
+++ pages/locators.py
@@ -4,12 +4,12 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#registration_link')
 
 class LoginPageLocators():
-    EMAIL = (By.CSS_SELECTOR, 'id_login-username')
-    PASSWORD = (By.CSS_SELECTOR, 'id_login-password')
+    EMAIL = (By.ID, 'id_login-username')
+    PASSWORD = (By.ID, 'id_login-password')
     I_FORGOT_PASSWORD_LINK = (By.LINK_TEXT, 'Я забыл пароль')
     SIGN_IN = (By.NAME, 'login_submit')
 
-    REGISTRATION_EMAIL = (By.ID, '#id_registration-email')
+    REGISTRATION_EMAIL = (By.ID, 'id_registration-email')
     REGISTRATION_PASSWORD = (By.ID, 'id_registration-password1')
     REGISTRATION_PASSWORD_REPEAT = (By.ID, 'id_registration-password2')
     REGISTRATION_BUTTON = (By.NAME, 'registration_submit')
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from locators import LoginPageLocators
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -13,15 +13,15 @@ class LoginPage(BasePage):
         assert 'login' in current_url, 'login is not in url'
 
     def should_be_login_form(self):
-        email = self.browser(*LoginPageLocators.EMAIL)
-        password = self.browser(*LoginPageLocators.PASSWORD)
-        sign_in_button = self.browser(*LoginPageLocators.SIGN_IN)
-        assert email and password and sign_in_button, 'page is broken, this id no login form'
+        email = self.is_element_present(*LoginPageLocators.EMAIL)
+        password = self.is_element_present(*LoginPageLocators.PASSWORD)
+        sign_in_button = self.is_element_present(*LoginPageLocators.SIGN_IN)
+        assert email and password and sign_in_button, f'page is broken, this is no login form {email, password, sign_in_button}'
 
     def should_be_register_form(self):
-        email = self.browser(*LoginPageLocators.REGISTRATION_EMAIL)
-        password = self.browser(*LoginPageLocators.REGISTRATION_PASSWORD)
-        password_repeat = self.browser(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT)
-        registration_button = self.browser(*LoginPageLocators.REGISTRATION_BUTTON)
-        assert email and password and registration_button and password_repeat, 'page is broken, this id no login form'
+        email = self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL)
+        password = self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD)
+        password_repeat = self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT)
+        registration_button = self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON)
+        assert email and password and registration_button and password_repeat, 'page is broken, this is no registration form'
 
